7cfb0383ece13ab98f86c423ba0d05111f9fa559
Alejandro Lazo Hernandez <Alejandro.Lazo@openlink.com>
2018-08-02 12:55:31 -0400

    Add Models and initial migration

---
+++ RPRAssistant/Models/CleaningSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RPRAssistant.Models
+{
+	public class CleaningSchedule
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public int PersonId { get; set; }
+
+		[ForeignKey("PersonId")]
+		public Person Person { get; set; }
+
+		public DateTime SchedDate { get; set; }
+
+		public bool EmailedFlag { get; set; }
+	}
+}
---
+++ RPRAssistant/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RPRAssistant.Models
+{
+	public class Person
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string EmailAddress { get; set; }
+	}
+}
